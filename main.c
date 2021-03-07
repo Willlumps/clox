9002c03a29bbb179e0f1b368b4a7d23d54e4c814
Nathan Willer <nathanielwiller93@gmail.com>
2021-03-07 17:16:56 -0500

    base VM

--- main.c
+++ main.c
@@ -2,22 +2,28 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 //TODO Fix line counts in disassembly
 
 int main(int argc, const char* argv[]) {
-    Chunk chunk;
-    initChunk(&chunk);
+  initVM();
 
-    // Stores the index of the added constant
-    writeConstant(&chunk, 1.2, 123);
-    writeConstant(&chunk, 1.3, 124);
-    writeConstant(&chunk, 1.4, 124);
-    writeConstant(&chunk, 1.5, 125);
+  Chunk chunk;
+  initChunk(&chunk);
 
-    writeChunk(&chunk, OP_RETURN, 125);
-    writeChunk(&chunk, OP_RETURN, 125);
-    disassembleChunk(&chunk, "test");
-    freeChunk(&chunk);
-    return 0;
+  // Stores the index of the added constant
+  writeConstant(&chunk, 155, 10);
+  writeConstant(&chunk, 1.2, 123);
+  writeConstant(&chunk, 1.3, 124);
+  writeConstant(&chunk, 1.4, 124);
+  writeConstant(&chunk, 1.5, 125);
+
+  writeChunk(&chunk, OP_RETURN, 125);
+  writeChunk(&chunk, OP_RETURN, 125);
+  disassembleChunk(&chunk, "test");
+  interpret(&chunk);
+  freeVM();
+  freeChunk(&chunk);
+  return 0;
 }
